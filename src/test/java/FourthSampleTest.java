3a2790aaf4be34381d0adbd5b71b2a60374183fc
Alena Zubrevich <Alena_Zubrevich@epam.com>
2017-11-10 15:12:40 +0300

    add new test

--- src/test/java/FourthSampleTest.java
+++ src/test/java/FourthSampleTest.java
@@ -1,28 +1,20 @@
 import com.epam.jira.JIRATestKey;
-import com.epam.jira.testng.ExecutionListener;
-import com.epam.jira.util.JiraInfoProvider;
 import org.testng.Assert;
-import org.testng.annotations.Listeners;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 
 public class FourthSampleTest {
-    private final Random random = new Random();
+
+    @BeforeClass
+    public void failedInit() {
+        throw new RuntimeException("Cannot find config file.");
+    }
 
     @JIRATestKey(key = "EPMFARMATS-827")
-    @Test ()
-    public void testMethod() throws InterruptedException {
-        boolean r = random.nextBoolean();
-        TimeUnit.SECONDS.sleep(2);
-
-        JiraInfoProvider.saveValue("Value1", "Sample");
-        JiraInfoProvider.saveValue("Value2", "Sample");
-        JiraInfoProvider.saveValue("Value3", "Sample");
+    @Test
+    public void blockedDueToFailedConfigsTest() {
         Assert.assertTrue(true);
-
     }
 
 }
